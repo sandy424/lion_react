0d46e7c8609840970cacd1cf6c6910618114a59a
sandy424@naver.com <sandy424@naver.com>
2026-01-13 10:51:52 +0900

    스프레드 연산

--- Chapter3/chap03.js
+++ Chapter3/chap03.js
@@ -2,5 +2,40 @@
 
 let person = {
     name: "Alice",
-    
-}
+    age: 20,
+    city: "New York",
+    job: "Nurse"
+};
+
+const { age, name, ...rest} = person;
+
+console.log(rest);
+// 배열의 구조 분해 할당
+
+const colors = ["red", "blue", "green"];
+
+// const firstColor = colors[0];
+// const secondColor = colors[1];
+
+const [ firstColor, ...res] = colors;
+
+console.log(firstColor);
+
+console.log(res);
+
+// 객체의 스프레드 연산
+const person1 = {name: "Bob", age: 23};
+const person2 = {
+    ...person1,
+    name: "Sandy",
+    city: "Busan",
+};
+
+console.log(person1);
+console.log(person2);
+
+// 배열의 스프레드 연산
+const fruits = ["Apple", "Banana"];
+const fruits2 = [...fruits, "Mango"];
+
+console.log(fruits2);
